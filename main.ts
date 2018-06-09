34028dadef4ee2d45fc4b91f681b74737dc21e73
fabio guelfi <fabioguelfiunix@gmail.com>
2018-06-08 22:35:59 -0300

    Restify

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-console.log('mainscript')
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0',
+});
+
+server.get('/hello', (req, res, next) => {
+
+    res.json({ message: 'hello world' })
+    return next();
+});
+
+server.listen(3000, () => {
+    console.log('api is running on http://localhost:3000')
+})
