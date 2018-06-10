22742676c7beb1bc581083f2251e157ec8d8f8cf
fabio guelfi <fabioguelfiunix@gmail.com>
2018-06-10 02:13:49 -0300

    Controlando Diferentes Versões na API - Restify

--- users/users.router.ts
+++ users/users.router.ts
@@ -1,27 +1,39 @@
-import {ModelRouter} from '../common/model-router'
+import { ModelRouter } from '../common/model-router'
 import * as restify from 'restify'
-import {NotFoundError} from 'restify-errors'
-import {User} from './users.model'
+import { NotFoundError } from 'restify-errors'
+import { User } from './users.model'
 
 
 class UsersRouter extends ModelRouter<User> {
 
-  constructor(){
+  constructor() {
     super(User)
-    this.on('beforeRender', document=>{
+    this.on('beforeRender', document => {
       document.password = undefined
       //delete document.password
     })
   }
 
-  applyRoutes(application: restify.Server){
+  findByEmail = (req, resp, next) => {
+    if (req.query.email) {
+      User.find({ email: req.query.email })
+        .then(this.renderAll(resp, next))
+        .catch(next())
+    } else {
+      next()
+    }
+  }
 
-    application.get('/users', this.findAll)
+  applyRoutes(application: restify.Server) {
+
+    application.get({ path: '/users', version: '2.0.0' }, [this.findByEmail, this.findAll])
+
+    application.get({ path: '/users', version: '1.0.0' }, this.findAll)
     application.get('/users/:id', [this.validateId, this.findById])
     application.post('/users', this.save)
-    application.put('/users/:id', [this.validateId,this.replace])
-    application.patch('/users/:id', [this.validateId,this.update])
-    application.del('/users/:id', [this.validateId,this.delete])
+    application.put('/users/:id', [this.validateId, this.replace])
+    application.patch('/users/:id', [this.validateId, this.update])
+    application.del('/users/:id', [this.validateId, this.delete])
 
   }
 }
